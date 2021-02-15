27eeb809a7d5d3beaaa4124aff58f63491bb98f2
Paulo Matias <matias@ufscar.br>
2021-02-15 11:06:54 -0300

    add caching

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_caching import Cache
 app = Flask(__name__)
+cache = Cache(config={'CACHE_TYPE': 'simple'})
+cache.init_app(app)
 CORS(app)
 
 @app.route('/')
