2727561882711699b382ec8be08aeffedbe53288
Paulo Matias <matias@ufscar.br>
2021-02-15 11:00:16 -0300

    enable CORS

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return jsonify({"test": "hi"})
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
